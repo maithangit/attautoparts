10fa681a68fcf4849925de3c3342ff99412d44c6
Thang Mai <mhthang.it@gmail.com>
2019-08-14 23:19:45 +0700

    Add resource js, css & header, footer

---
+++ web/resources/script/script.js
@@ -0,0 +1,32 @@
+
+$(function () {
+
+    $("#btnRegister").click(register);
+
+    function register() {
+        /*Tam Tan*/
+    }
+
+    $('.addToCart').click(function () {
+        /*Alican*/
+    });
+
+    $('#btnContinueShopping').click(function () {
+        $(location).attr("href", "/");
+    });
+
+    $('#btnRemove, #btnUpdate').click(function () {
+        /*Alican*/
+
+    });
+    function reloadPage(){
+        location.reload();
+    }
+
+    $("#btnCheckout").click(()=>{
+        /*Alican*/
+    });
+
+
+
+})

954d197d0e60d45f17c1f5d7f385f52a51dd8292
tamtan <vinhtam92@gmail.com>
2019-08-15 10:09:48 -0500

    - Define user

--- src/main/java/att/group/dao/UserDAO.java
+++ src/main/java/att/group/dao/UserDAO.java
@@ -14,6 +14,11 @@ public class UserDAO {
     {
         usersDB = new HashMap<>();
         //Define users
+        usersDB = new HashMap<>();
+        usersDB.put("Tam", new User(1, "Tam", "123456", "Vinh Tam Tan", "vtan@mum.edu", "2000 N. Court St., Fairfield, Iowa", "(641) 233 - 9000"));
+        usersDB.put("Thang", new User(2, "Thang","123456", "Mai Thang", "mthang@mum.edu", "301 Main St., Fairfield, Iowa", "(641) 656 - 8008"));
+        usersDB.put("Ali", new User(3, "Alican Ozer","123456", "", "aozer@mum.edu", "301 Main St., Fairfield, Iowa", "(641) 644 - 8008"));
+
     }
 
     public boolean checkExistUser(String username, String password){
